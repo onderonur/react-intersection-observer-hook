915db61bae3d704cf1b8e3d02887775107e265b1
onderonur <onuronder.onr@gmail.com>
2019-12-05 02:30:41 +0300

    example fix

--- example/index.tsx
+++ example/index.tsx
@@ -1,7 +1,7 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import useIntersectionObserver from '../src';
+import { useIntersectionObserver } from '../src';
 import styled from 'styled-components';
 
 const Root = styled.div`
